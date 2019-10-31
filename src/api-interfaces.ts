1e20b4f186bdd3c553d5817b1e7f9516140fdcb9
DW <chocological00@outlook.com>
2019-10-31 21:07:17 +1100

    자잘한거

--- src/api-interfaces.ts
+++ src/api-interfaces.ts
@@ -1,32 +1,34 @@
-export interface APIQuestion {
-    _id: string;
-    updatedAt: string;
-    createdAt: string;
-    user: APIUser;
-    question: string;
-    isNSFW: boolean;
-    likesCount: number;
-    isDeleted: boolean;
+export interface APIQuestion
+{
+	_id: string;
+	updatedAt: string;
+	createdAt: string;
+	user: APIUser;
+	question: string;
+	isNSFW: boolean;
+	likesCount: number;
+	isDeleted: boolean;
 
-    questionUser: APIUser | undefined;
-    answer: string | undefined;
-    answeredAt: string | undefined;
+	questionUser: APIUser | undefined;
+	answer: string | undefined;
+	answeredAt: string | undefined;
 }
 
-export interface APIUser {
-    _id: string;
-    updatedAt: string;
-    createdAt: string;
-    name: string;
-    acct: string;
-    acctDisplay: string;
-    avatarUrl: string;
-    url: string;
-    allAnon: boolean;
-    questionBoxName: string | undefined;
-    description: string | undefined;
-    hostName: string;
-    pushbulletEnabled: boolean;
-    isTwitter: boolean;
-    stopNewQuestion: boolean;
+export interface APIUser
+{
+	_id: string;
+	updatedAt: string;
+	createdAt: string;
+	name: string;
+	acct: string;
+	acctDisplay: string;
+	avatarUrl: string;
+	url: string;
+	allAnon: boolean;
+	questionBoxName: string | undefined;
+	description: string | undefined;
+	hostName: string;
+	pushbulletEnabled: boolean;
+	isTwitter: boolean;
+	stopNewQuestion: boolean;
 }

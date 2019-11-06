0701aab279b9cd2100b21589502432d9b20ecdc3
DW <chocological00@outlook.com>
2019-11-06 21:44:38 +1100

    빌드 안되는 문제 일단 전부 수정

--- src/client/components/question.tsx
+++ src/client/components/question.tsx
@@ -1,4 +1,4 @@
-import * as moment from 'moment';
+import moment from 'moment';
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Card, CardBody, CardSubtitle, CardText, CardTitle, FormGroup, Input } from 'reactstrap';

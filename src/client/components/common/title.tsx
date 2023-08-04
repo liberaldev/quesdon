fdd2bfdc52be5faa8c4b4926f54c14b46aad5038
liberaldev <ssharp0614@gmail.com>
2023-08-04 19:21:58 +0900

    Children이 먼저 임포트 되도록 수정

--- src/client/components/common/title.tsx
+++ src/client/components/common/title.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { Helmet } from 'react-helmet';
 import { Children } from 'react';
+import { Helmet } from 'react-helmet';
 
 export class Title extends React.Component 
 {
